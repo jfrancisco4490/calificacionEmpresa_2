83fd28dd8566fea4f015e6deb10016bdf90f5e4c
Abel Josué Francisco Agra <jfrancisco4490@gmail.com>
2015-11-25 10:41:59 +0100

    Cambios en estructura del proyecto

--- config/config_bd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ../db
-# Crear archivo con base de datos sqlite3 en sistema de archivos
-sqlite3 CC_calificacionEmpresa.db < creacionTablasBD.sql
-cd ..
-# Ejecutar tests unitarios
-mocha
