ec558857e6e74a68069a03f81242a8f7501f7de8
Abel Josué Francisco Agra <jfrancisco4490@gmail.com>
2015-11-25 11:30:34 +0100

    Cambio en archivo de configuracion para despliegue

--- config/config_bd.sh
+++ config/config_bd.sh
@@ -3,5 +3,3 @@ cd db
 # Crear archivo de base de datos sqlite3 en el sistema de archivos (y cargar el esquema)
 sqlite3 CC_calificacionEmpresa.db < creacionTablasBD.sql
 cd ..
-# Ejecutar tests unitarios
-mocha
