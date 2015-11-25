48cd786a54284a7099396ec30d83b7ffd8e0570e
Abel Josué Francisco Agra <jfrancisco4490@gmail.com>
2015-11-25 10:55:48 +0100

    Incluido directorio y primer archivo de configuracion

--- config/config_bd.sh
+++ config/config_bd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ../db
+cd db
 # Crear archivo de base de datos sqlite3 en el sistema de archivos (y cargar el esquema)
 sqlite3 CC_calificacionEmpresa.db < creacionTablasBD.sql
 cd ..
